2f260b38e9ad6b7bc0b0b8ae9a7045cb8558c8e9
ramondcosta <ramon.eumsm.costa@gmail.com>
2016-09-20 15:09:29 -0300

    Code cleanning

--- temp.R
+++ temp.R
@@ -1,15 +1,16 @@
 dados <- data.frame(read.table("./data/ENEM_LEVE.csv", sep=",", header=TRUE))
+
+#transform the collunms in numerica data
 dados$IDADE = as.numeric(as.character(dados$IDADE))
 dados$ANO_CONCLUIU = as.numeric(as.character(dados$ANO_CONCLUIU))
+
+#store the conditionals
 index =((dados$IDADE > 100) | (dados$IDADE < 10))
 #dados = dados[index, ]
+
+# Normalize the outliers age
 dados[index,c("IDADE")] <- 17 + 2012 - dados[index,c("ANO_CONCLUIU")]
+
+# Plot!
 boxplot(na.omit(as.numeric(as.character(dados$IDADE))), main="", xlab="", ylab="Nota Matem?tica")
-#dados <- ifelse(!is.na(dados$ANO_CONCLUIU), 17 + 2012 - dados$ANO_CONCLUIU , dados$IDADE)
-#idade <- dados[!is.na(as.numeric(as.character(dados$IDADE))) & (dados$IDADE < 100) & (dados$IDADE > 10), c("IDADE")]
-#dados[(( (as.numeric(as.character(dados$IDADE))) < 100) | ( (as.numeric(as.character(dados$IDADE))) > 10)),dados$IDADE] <- "17"
-#idade <- dados[!is.na(as.numeric(as.character(dados$IDADE))) , c("IDADE")]
-#idade[((idade$IDADE < 100) | (idade$IDADE > 10)),idade$IDADE] = 17 #<- transform(idade, IDADE=ifelse(((IDADE < 100) | (IDADE > 10)),17, IDADE) )
-#idade <- transform(idade, "IDADE"=17 )
-#print(idade)
-#boxplot(na.omit(as.numeric(as.character(idade))), main="", xlab="", ylab="Nota Matem?tica")
+
