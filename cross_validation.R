14104886682ead170e50482ae81a6e2b28ddd831
Mateus Cordeiro <mateus_cgc@hotmail.com>
2016-10-20 00:20:57 -0300

    Adiciona metrica: Confusion Matrix
    Melhora escolha das colunas removidas

--- cross_validation.R
+++ cross_validation.R
@@ -1,6 +1,11 @@
 # Biblioteca com implementação do Naive Bayes
 library(e1071)
 
+same_class <- function(a, b) {
+  return (   ((a == 'P' || a == 'F') && (b == 'P' || b == 'F'))
+     || ((a != 'P' && a != 'F') && (b != 'P' && b != 'F')))
+}
+
 dados <- read.table("./processed/processed.csv", sep=",", header=TRUE)
 
 folds <- 1:10
@@ -20,10 +25,31 @@ for(i in folds) {
 
   erros[i] = 0;
 
+  cols <- c()
+  rows <- c()
+
   for(r in 1:length(pred)) {
-      if(pred[r] != test_set[r, "IN_STATUS_REDACAO"])
-        erros[i] = erros[i] + 1
+    if(pred[r] != test_set[r, "IN_STATUS_REDACAO"])
+      erros[i] = erros[i] + 1
+
+    # numeric_column(pred)
+
+    if(test_set[r, "IN_STATUS_REDACAO"] == 'P' || test_set[r, "IN_STATUS_REDACAO"] == 'F')
+      cols[r] = 'Ñ deu merda'
+    else
+      cols[r] = 'Deu merda'
+
+    if(same_class(pred[r], test_set[r, "IN_STATUS_REDACAO"])) {
+      if(pred[r] == test_set[r, "IN_STATUS_REDACAO"])
+        rows[r] = 'classificou corretamente'
+      else
+        rows[r] = 'classificou corretamente a classe'
+    } else
+      rows[r] = 'classificou incorretamente'
   }
+  print(length(cols))
+  print(length(rows))
+  print(table(rows, cols))
 
   print(table(pred, test_set[, "IN_STATUS_REDACAO"]))
   # erros[i] = mean(pred, test_set[, "IN_STATUS_REDACAO"])
