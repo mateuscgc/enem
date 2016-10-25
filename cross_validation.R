e1ae7ecaa54d5fd1fa63cff9e3149fe00131f22e
Mateus Cordeiro <mateus_cgc@hotmail.com>
2016-10-25 12:48:47 -0300

    final

--- cross_validation.R
+++ cross_validation.R
@@ -2,8 +2,7 @@
 library(e1071)
 
 same_class <- function(a, b) {
-  return (   ((a == 'P' || a == 'F') && (b == 'P' || b == 'F'))
-     || ((a != 'P' && a != 'F') && (b != 'P' && b != 'F')))
+  return ((a == 'P' && b == 'P') || (a != 'P' && b != 'P'))
 }
 
 dados <- read.table("./processed/processed.csv", sep=",", header=TRUE)
@@ -27,6 +26,7 @@ for(i in folds) {
 
   cols <- c()
   rows <- c()
+  corrc <- c()
 
   for(r in 1:length(pred)) {
     if(pred[r] != test_set[r, "IN_STATUS_REDACAO"])
@@ -34,19 +34,34 @@ for(i in folds) {
 
     # numeric_column(pred)
 
-    if(test_set[r, "IN_STATUS_REDACAO"] == 'P' || test_set[r, "IN_STATUS_REDACAO"] == 'F')
+    if(test_set[r, "IN_STATUS_REDACAO"] == 'P')
       cols[r] = 'Ñ deu merda'
     else
       cols[r] = 'Deu merda'
 
     if(same_class(pred[r], test_set[r, "IN_STATUS_REDACAO"])) {
-      if(pred[r] == test_set[r, "IN_STATUS_REDACAO"])
+      if(pred[r] == test_set[r, "IN_STATUS_REDACAO"]) {
         rows[r] = 'classificou corretamente'
-      else
+        corrc[r] = 'Predição correta'
+      } else {
         rows[r] = 'classificou corretamente a classe'
-    } else
+        corrc[r] = 'Predição incorreta'
+      }
+    } else {
       rows[r] = 'classificou incorretamente'
+      corrc[r] = 'Predição incorreta'
+    }
   }
+  # corrc <- merge(corrc, test_set[, "IN_STATUS_REDACAO"])
+  stacks <- table(corrc, test_set[, "IN_STATUS_REDACAO"])
+
+  # ColNums <- grep("P", names(stacks))
+
+  print(stacks)
+  barplot(stacks, main="Distribuição de predições por status e validade",
+          xlab="Status da redação", col=c("white","grey"),
+          legend = rownames(stacks))
+
   print(length(cols))
   print(length(rows))
   print(table(rows, cols))
@@ -57,3 +72,4 @@ for(i in folds) {
 }
 
 print(erros)
+print(mean(erros))
